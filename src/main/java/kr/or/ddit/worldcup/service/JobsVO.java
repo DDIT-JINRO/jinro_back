2513fefd095361d4c01294d492fa58dc6eb85a31
NZM <nzm6846@gmail.com>
2025-07-21 23:54:50 +0900

    feat : 디비 컬럼 변경으로 jobsVO 수정

--- src/main/java/kr/or/ddit/worldcup/service/JobsVO.java
+++ src/main/java/kr/or/ddit/worldcup/service/JobsVO.java
@@ -3,17 +3,27 @@ package kr.or.ddit.worldcup.service;
 import java.util.Date;
 
 public class JobsVO {
-	private int jobId;
 	private String jobCode;
 	private String jobName;
 	private String jobLcl;
 	private String jobMcl;
-	private int jobSal;
-	private String jobSatis;
-	private String jobProspect;
-	private String jobReqEdu;
+	private String jobSal;
+	private int jobSatis;
+	private String jobWay;
+	private String jobRelatedMajor;
 	private String jobRelatedCert;
 	private String jobMainDuty;
 	private Date jobCreatedAt;
 	private int fileGroupNo;
+	private int edubgMgraduUndr;
+	private int edubgHgradu;
+	private int edubgCgraduUndr;
+	private int edubgUgradu;
+	private int edubgGgradu;
+	private int edubgDgradu;
+	private int outlookIncrease;
+	private int outlookSlightIncrease;
+	private int outlookStable;
+	private int outlookSlightDecrease;
+	private int outlookDecrease;
 }
