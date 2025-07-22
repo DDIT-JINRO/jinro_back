531ed313b8447761485b06fe129409a00e2660ed
NZM <nzm6846@gmail.com>
2025-07-22 16:02:22 +0900

    feat : 월드컵 직업목록 api, 1등 직업 api 컨트롤러 구현

--- src/main/java/kr/or/ddit/worldcup/web/WorldCupController.java
+++ src/main/java/kr/or/ddit/worldcup/web/WorldCupController.java
@@ -6,11 +6,14 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.ddit.worldcup.service.ComCodeVO;
+import kr.or.ddit.worldcup.service.JobsVO;
 import kr.or.ddit.worldcup.service.WorldCupService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,12 +27,41 @@ public class WorldCupController {
 	private final WorldCupService worldCupService;
 	
 	@GetMapping("/selectCategories")
-	public ResponseEntity<List<ComCodeVO>> selectCategories(@RequestParam String round){
+	public ResponseEntity<List<ComCodeVO>> selectCategories(@RequestParam String round) {
 		List<ComCodeVO> comCodeVOList = worldCupService.selectCategories(round);
-		log.info("WorldCupController - > selectCategories : "+comCodeVOList);
+		log.info("WorldCupController - > selectCategories : " + comCodeVOList);
 		return ResponseEntity.ok(comCodeVOList);
-		
+
 	}
+
+	@PostMapping("/selectJobsByCategory")
+	public ResponseEntity<List<JobsVO>> selectJobsByCategory(@RequestBody ComCodeVO comCodeVO) {
+		log.info("WorldCupController -> comCodeVO :" + comCodeVO);
+		List<JobsVO> jobsVOList = worldCupService.selectJobsByCategory(comCodeVO);
+		log.info("WorldCupController -> jobsVOList :" + jobsVOList);
+
+		return ResponseEntity.ok(jobsVOList);
+
+	}
+	
+	@PostMapping("/selectJobById")
+	public ResponseEntity<JobsVO> selectJobById(@RequestBody JobsVO jobsVO) {
+		jobsVO = worldCupService.selectJobById(jobsVO);
+		log.info("selectJobById -> jobsVO :" + jobsVO);
+		
+		return ResponseEntity.ok(jobsVO);
+
+	}
+	
+//	@PostMapping("/insertWorldcupResult")
+//	public ResponseEntity<int> insertWorldcupResult(@RequestBody JobsVO jobsVO) {
+//
+//		int cnt = worldCupService.insertWorldcupResult(jobsVO);
+//		
+//		return ResponseEntity.ok();
+//
+//	}
+	
 	
 	
 }
