d121d1983c77fe511a11218b8da37fa7b6b007c5
jungheol <jungheol@naver.com>
2025-07-24 15:33:04 +0900

    feat : 면접 리스트 가져와서 option으로 선택할 수 있도록 구현

--- src/main/java/kr/or/ddit/cdp/imtintrvw/aiimtintrvw/service/impl/AiImitationInterviewServiceImpl.java
+++ src/main/java/kr/or/ddit/cdp/imtintrvw/aiimtintrvw/service/impl/AiImitationInterviewServiceImpl.java
@@ -1,5 +1,22 @@
 package kr.or.ddit.cdp.imtintrvw.aiimtintrvw.service.impl;
 
-public class AiImitationInterviewServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.cdp.imtintrvw.aiimtintrvw.service.AiImitationInterviewService;
+import kr.or.ddit.cdp.imtintrvw.aiimtintrvw.service.InterviewDetailListVO;
+
+@Service
+public class AiImitationInterviewServiceImpl implements AiImitationInterviewService {
+
+	@Autowired
+	InterviewDetailListMapper interviewDetailListMapper; 
+	
+	@Override
+	public List<InterviewDetailListVO> getQuestionList() {
+		return interviewDetailListMapper.getQuestionList();
+	}
 
 }
--- src/main/java/kr/or/ddit/cdp/imtintrvw/aiimtintrvw/web/AiImitationInterviewController.java
+++ src/main/java/kr/or/ddit/cdp/imtintrvw/aiimtintrvw/web/AiImitationInterviewController.java
@@ -1,19 +1,38 @@
 package kr.or.ddit.cdp.imtintrvw.aiimtintrvw.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.or.ddit.cdp.imtintrvw.aiimtintrvw.service.AiImitationInterviewService;
+import kr.or.ddit.cdp.imtintrvw.aiimtintrvw.service.InterviewDetailListVO;
+import kr.or.ddit.cdp.imtintrvw.bsintrvw.web.BassInterviewController;
 import lombok.extern.slf4j.Slf4j;
 
 @RequestMapping("/cdp/imtintrvw/aiimtintrvw")
 @Controller
 @Slf4j
 public class AiImitationInterviewController {
-	
+
+	@Autowired
+	AiImitationInterviewService aiImitationInterviewService;
+
 	@GetMapping("/aiImitationInterview.do")
 	public String aiImitationInterviewPage() {
 		return "cdp/imtintrvw/aiimtintrvw/aiImitationInterview";
+	}
+	
+	@GetMapping("/getQuestionLists")
+	@ResponseBody
+	public List<InterviewDetailListVO> getQuestionList() {
+		List<InterviewDetailListVO> list = aiImitationInterviewService.getQuestionList();
+		log.info("list : " + list);
+		
+		return aiImitationInterviewService.getQuestionList();
 	}
 	
 	@GetMapping("/detail.do")
